663c224eb60b62afbefe9498fcfa208855a578bb
Nikaidou Haruki <nharuki@pm.me>
2026-04-01 20:57:10 +0900

    prepare for remote function wrapper

--- svelte.config.js
+++ svelte.config.js
@@ -11,13 +11,19 @@ const config = {
 			const isExternalLibrary = pathSegments.includes('node_modules');
 
 			return isExternalLibrary ? undefined : true;
+		},
+		experimental: {
+			async: true,
 		}
 	},
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		experimental: {
+			remoteFunctions: true,
+		}
 	}
 };
 
